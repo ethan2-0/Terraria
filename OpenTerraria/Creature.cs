98ebc00209e817161d3a140a9f59293861288ef3
Ethan White <ethanwhite@rogers.com>
2014-02-25 12:41:28 -0800

    Continued work on Entities

--- OpenTerraria/Creature.cs
+++ OpenTerraria/Creature.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace OpenTerraria {
-    public abstract class Creature {
-
+    public abstract class Creature : Entity {
+        public abstract int getMaxHealth();
+        int health;
+        public Creature(String imageName, Point location) : base(imageName, location) {
+            health = getMaxHealth();
+        }
+        public void damage(int amount) {
+            health -= amount;
+        }
     }
 }
