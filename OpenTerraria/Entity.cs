b8384b0c860602571b22a63196628a5888218d59
Ethan White <ethanwhite@rogers.com>
2014-02-25 12:35:59 -0800

    Started working on entities

--- OpenTerraria/Entity.cs
+++ OpenTerraria/Entity.cs
@@ -8,12 +8,20 @@ namespace OpenTerraria {
     public abstract class Entity {
         public Bitmap image;
         public Point location;
+        public PointF momentum;
         public Entity(String imageName, Point location) {
             this.image = Reference.getImage(imageName);
             this.location = location;
+            momentum = new PointF(0, 0);
         }
         public void draw(Graphics g) {
-
+            g.DrawImage(image, location);
+        }
+        public void move() {
+            location = Util.addPoints(location, Util.convertToPoint(momentum));
+        }
+        public void stop() {
+            momentum = new PointF(0, 0);
         }
     }
 }
--- OpenTerraria/Util.cs
+++ OpenTerraria/Util.cs
@@ -9,5 +9,11 @@ namespace OpenTerraria {
         public static Point addPoints(Point p1, Point p2) {
             return new Point(p1.X + p2.X, p1.Y + p2.Y);
         }
+        public static PointF addPoints(PointF p1, PointF p2) {
+            return new PointF(p1.X + p2.X, p1.Y + p2.Y);
+        }
+        public static Point convertToPoint(PointF p) {
+            return new Point((int)p.X, (int)p.Y);
+        }
     }
 }
