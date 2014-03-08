0d155a15e0c6e6007ad3539e0befbac1383ce59e
Ethan White <ethanwhite@rogers.com>
2014-03-07 17:00:48 -0800

    Started work on crafting

---
+++ OpenTerraria/Recepie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenTerraria {
+    public class Recepie {
+        public Dictionary<InventoryItem, int> input;
+        public KeyValuePair<InventoryItem, int> output;
+        public Recepie(Dictionary<InventoryItem, int> input, KeyValuePair<InventoryItem, int> output) {
+            this.input = input;
+            this.output = output;
+        }
+    }
+}
